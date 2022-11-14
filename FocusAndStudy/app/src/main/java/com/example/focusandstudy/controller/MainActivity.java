f6a2ae44fcab0e011bc7a1534c72b35d7cdb56fb
flora <flora.nguyen@edu.esiee.fr>
2022-11-14 17:09:05 -0500

    correction

--- FocusAndStudy/app/src/main/java/com/example/focusandstudy/controller/MainActivity.java
+++ FocusAndStudy/app/src/main/java/com/example/focusandstudy/controller/MainActivity.java
@@ -1,10 +1,16 @@
-package com.example.focusandstudy;
+package com.example.focusandstudy.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.focusandstudy.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -17,21 +23,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-<<<<<<< HEAD:FocusAndStudy/app/src/main/java/com/example/focusandstudy/MainActivity.java
-
-=======
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_calendar_weekly, R.id.navigation_profile, R.id.navigation_settings )
                 .build();
->>>>>>> 4573de3 (Merge pull request #9 from NguyenFlora/feat/set_up_next_activities):FocusAndStudy/app/src/main/java/com/example/focusandstudy/controller/MainActivity.java
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
