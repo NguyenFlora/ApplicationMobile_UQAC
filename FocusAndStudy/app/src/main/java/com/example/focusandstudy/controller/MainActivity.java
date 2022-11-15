8b210afb07ac32de5c164cd7c0dd043215cd230b
flora <flora.nguyen@edu.esiee.fr>
2022-11-14 21:28:35 -0500

    lien vers Cycle Pomodoro

--- FocusAndStudy/app/src/main/java/com/example/focusandstudy/controller/MainActivity.java
+++ FocusAndStudy/app/src/main/java/com/example/focusandstudy/controller/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.focusandstudy.controller;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.focusandstudy.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -30,6 +34,18 @@ public class MainActivity extends AppCompatActivity {
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        Button newSession = (Button)findViewById(R.id.start_a_new_session);
+        newSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment= new PomodoroFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.container, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
     }
 
 }
